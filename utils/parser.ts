fd3122732d6bfdb52fce46d1325acfcd8ee4e161
akshitkrnagpal <akshitkrnagpal@gmail.com>
2023-09-24 08:13:45 +0530

    Fix logic for parsing options

--- utils/parser.ts
+++ utils/parser.ts
@@ -1,17 +1,14 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 const DEFAULT_WIDTH = 600;
 const DEFAULT_HEIGHT = 400;
 
 const optionsSchema = z.object({
-  width: z.coerce.number().default(DEFAULT_WIDTH).catch(DEFAULT_WIDTH),
-  height: z.coerce.number().default(DEFAULT_HEIGHT).catch(DEFAULT_HEIGHT),
+  width: z.coerce.number().positive().catch(DEFAULT_WIDTH),
+  height: z.coerce.number().positive().catch(DEFAULT_HEIGHT),
 });
 
 export const getPlaceholdOptions = (filename: string) => {
-  const options: Record<string, any> = {};
-
-  const [width, height] = filename.split("x");
-
-  return optionsSchema.parse({ width, height });
+  const [width, height] = filename.split('x');
+  return optionsSchema.parse({ width, height: height ?? width });
 };
