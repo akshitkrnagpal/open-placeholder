bf5a1f717a83529e4d609ebe05c8ffb27006f69b
akshitkrnagpal <akshitkrnagpal@gmail.com>
2023-09-24 08:05:29 +0530

    Fix image

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className='flex flex-col items-center p-24 gap-8 min-h-screen bg-[#31343C]'>
       <h1 className='text-8xl text-white'>Placehold</h1>
-      <Image src='/600x400' width={600} height={400} alt='' />
+      <Image unoptimized src='/600x400' width={600} height={400} alt='' />
     </main>
   );
 }
