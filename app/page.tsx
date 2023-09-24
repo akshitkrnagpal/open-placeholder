322a91f4b9d48f9d88a0e766d08d853b98d56d52
akshitkrnagpal <akshitkrnagpal@gmail.com>
2023-09-24 09:02:41 +0530

    Made landing page responsive

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className='flex flex-col items-center p-24 gap-8 min-h-screen bg-[#31343C]'>
-      <h1 className='text-8xl text-white'>Placehold</h1>
+    <main className='flex flex-col items-center py-12 px-6 md:p-24 gap-8 min-h-screen bg-[#31343C]'>
+      <h1 className='text-4xl md:text-8xl text-white'>Placehold</h1>
       <Image unoptimized src='/600x400' width={600} height={400} alt='' />
     </main>
   );
