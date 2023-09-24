3379e974f26ebc21fef5a38dc3fe963adc838d83
akshitkrnagpal <akshitkrnagpal@gmail.com>
2023-09-24 23:10:28 +0530

    Add priority to hero image

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,14 @@ export default function Home() {
   return (
     <main className='flex flex-col items-center py-12 px-6 md:p-24 gap-8 min-h-screen bg-[#31343C]'>
       <h1 className='text-4xl md:text-8xl text-white'>Placehold</h1>
-      <Image unoptimized src='/600x400' width={600} height={400} alt='' />
+      <Image
+        priority
+        unoptimized
+        src='/600x400'
+        width={600}
+        height={400}
+        alt=''
+      />
     </main>
   );
 }
