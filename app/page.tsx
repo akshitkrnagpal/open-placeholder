974f82210c97eec5953a767901ce4fc7672acdca
Akshit Kr Nagpal <akshitkrnagpal@gmail.com>
2023-10-11 03:42:44 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className='flex flex-col items-center py-12 px-6 md:p-24 gap-8 min-h-screen bg-[#31343C]'>
-      <h1 className='text-4xl md:text-8xl text-white'>Placehold</h1>
+      <h1 className='text-4xl md:text-8xl text-white'>Open Placeholder</h1>
       <Image
         priority
         unoptimized
